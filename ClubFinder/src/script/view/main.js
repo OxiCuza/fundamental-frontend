ae0e85f4eb9a291c87a81c72e8c53f5c379ac6ed
OxiCuza <oxicusa@gmail.com>
2020-05-07 13:16:58 +0700

    ES6 Export and Import syntax

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,23 +1,25 @@
+import DataSource from '../data/data-source.js';
+
 const main = () => {
 	const searchElement = document.querySelector("#searchElement");
 	const buttonSearchElement = document.querySelector("#searchButtonElement");
 	const clubListElement = document.querySelector("#clubList");
 
 	const onButtonSearchClicked = () => {
-		const dataSource = new DataSource(renderResult, fallbackResult);
-		dataSource.searchClub(searchElement.value);
+		DataSource.searchClub(searchElement.value)
+			.then(renderResult)
+			.catch(fallbackResult);
 	};
 
-	const renderResult = results => {
+	const renderResult = (results) => {
 		clubListElement.innerHTML = "";
-		results.forEach(club => {
-			const {name, fanArt, description} = club;
+		results.forEach((club) => {
+			const { name, fanArt, description } = club;
 
 			const clubElement = document.createElement("div");
 			clubElement.setAttribute("class", "club");
 
-			clubElement.innerHTML =
-				`<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+			clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
 				<div class="club-info">
 					<h2>${name}</h2>
 					<p>${description}</p>
@@ -26,11 +28,12 @@ const main = () => {
 		});
 	};
 
-	const fallbackResult = message => {
+	const fallbackResult = (message) => {
 		clubListElement.innerHTML = "";
-		clubListElement.innerHTML +=
-			`<h2 class="placeholder">${message}</h2>`;
+		clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
 	};
 
 	buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
+
+export default main;
