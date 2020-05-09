806f9497fdf043f4ba0a1eb74d5e1ddd074dce76
OxiCuza <oxicusa@gmail.com>
2020-05-09 11:39:14 +0700

    Solution: Membuat search-bar Component

---
+++ ClubFinder/src/script/component/search-bar.js
@@ -0,0 +1,26 @@
+class SearchBar extends HTMLElement {
+	connectedCallback() {
+		this.render();
+    }
+    
+    set clickEvent(event){
+        this._event = event;
+        this.render();
+    }
+
+    get value(){
+        return this.querySelector('#searchElement').value;
+    }
+
+	render() {
+		this.innerHTML = `
+        <div id="search-container" class="search-container">
+            <input placeholder="Search football club" id="searchElement" type="search" />
+            <button id="searchButtonElement" type="submit">Search</button>
+        </div>`;
+
+        this.querySelector('#searchButtonElement').addEventListener('click', this._event);
+	}
+}
+
+customElements.define("search-bar", SearchBar);
